c270ef3de8d27c80f69e64facafde9c7ee6be655
ShaurGaur <shauryavrat@live.com>
2020-12-15 22:24:17 -0800

    update aoc template

--- aoc2020/base/main.cpp
+++ aoc2020/base/main.cpp
@@ -4,11 +4,25 @@
 #define MOD 1000000007
 using namespace std;
 
+ull part1() {
+    return 69;
+}
+
+ull part2() {
+    return 420;
+}
+
 int main() {
-    ifstream fin("input.txt", ios::in);
+    string temp;
+    cin >> temp;
+    ifstream fin(temp, ios::in);
 
     while (!fin.eof()) {
     }
+    fin.close();
+
+    cout << "part 1: " << part1() << endl;
+    cout << "part 2: " << part2() << endl;
 
     return 0;
 }
