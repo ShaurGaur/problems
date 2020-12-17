776a8cd2b8c505372cbd2746ff6ff7634adc7cd9
ShaurGaur <shauryavrat@live.com>
2020-12-16 22:30:06 -0800

    made updating active easier

--- aoc2020/17/main.cpp
+++ aoc2020/17/main.cpp
@@ -8,28 +8,28 @@ set<vector<int>> active;
 set<vector<int>> active0;
 int n = 0;
 
+void updateActive(map<vector<int>, int>& freqs) {
+    set<vector<int>> active2;
+    for (auto p : freqs) {
+        if (p.second == 3)
+            active2.insert(p.first);
+        else if (p.second == 2 && active.find(p.first) != active.end())
+            active2.insert(p.first);
+    }
+    active = active2;
+}
+
 ull part1() {
    for (int t = 0; t < 6; t++) {
         set<vector<int>> active2;
-        map<vector<int>, int> freqs;
-        
         for (vector<int> v : active) {
-            int j = 0;
             for (int x = (v[0] - 1); x <= (v[0] + 1); x++)
             for (int y = (v[1] - 1); y <= (v[1] + 1); y++)
             for (int z = (v[2] - 1); z <= (v[2] + 1); z++)
             if (x != v[0] || y != v[1] || z != v[2])
                 freqs[{x, y, z, 0}]++;
         }
-
-        for (auto p : freqs) {
-            if (p.second == 3)
-                active2.insert(p.first);
-            else if (p.second == 2 && active.find(p.first) != active.end())
-                active2.insert(p.first);
-        }
-        
-        active = active2;
+        updateActive(freqs);
     }
 
     return active.size(); 
@@ -37,9 +37,7 @@ ull part1() {
 
 ull part2() {
    for (int t = 0; t < 6; t++) {
-        set<vector<int>> active2;
         map<vector<int>, int> freqs;
-
         for (vector<int> v : active) {
             for (int x = (v[0] - 1); x <= (v[0] + 1); x++)
             for (int y = (v[1] - 1); y <= (v[1] + 1); y++)
@@ -48,15 +46,7 @@ ull part2() {
             if (x != v[0] || y != v[1] || z != v[2] || w != v[3])
                 freqs[{x, y, z, w}]++;
         }
-
-        for (auto p : freqs) {
-            if (p.second == 3)
-                active2.insert(p.first);
-            else if (p.second == 2 && active.find(p.first) != active.end())
-                active2.insert(p.first);
-        }
-
-        active = active2;
+        updateActive(freqs);
     }
 
     return active.size(); 
