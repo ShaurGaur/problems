52e67d8e0afa730d86f77d225cc161756fedaaac
ShaurGaur <shauryavrat@live.com>
2021-01-16 13:27:22 -0800

    minor changes to cf1473c

--- codeforces/1473/C.cpp
+++ codeforces/1473/C.cpp
@@ -12,9 +12,8 @@ vector<int> solve(int n, int k) {
 
     if (n > k) {
         int d = 2*k - n;
-        for (int i = d; i <= k; i++) {
+        for (int i = d; i <= k; i++)
             p[i-1] = k - (i-d);
-        }
     }
     
     return p;
@@ -26,16 +25,10 @@ int main() {
     vector<vector<int>> ans;
     for (int i = 0; i < t; i++) {
         cin >> n >> k;
-        // ans.push_back(solve(n, k));
         vector<int> v = solve(n, k);
         for (int i : v) cout << i << " ";
         cout << endl;
     }
-
-    // for (vector<int> v : ans) {
-    //     for (int i : v) cout << i << " ";
-    //     cout << endl;
-    // }
 
     return 0;
 }
