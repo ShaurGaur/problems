ec17b9c8432fbac42de7861c59bd91644478838e
ShaurGaur <shauryavrat@live.com>
2021-02-20 11:43:25 -0800

    minor changes to d1 2019 d

--- codeforces/d1-2019/D/main.py
+++ codeforces/d1-2019/D/main.py
@@ -1,11 +1,8 @@
 def recurse(a, b):
-    if a <= b:
-        return b - a
-    
-    if a % 2 == 0:
-        return 1 + recurse(a // 2, b)
-    else:
-        return 1 + recurse(a + 1, b)
+    if a <= b: return b - a
+    step = 1 + (a % 2)
+    a = a + (a % 2)
+    return step + recurse(a // 2, b)
 
 a, b = map(int, input().split(" "))
 print(recurse(a, b))
