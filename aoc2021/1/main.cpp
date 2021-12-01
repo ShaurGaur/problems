dbe6c0cdb048fb3919182e2b559a5a7ff3182cdf
ShaurGaur <shauryavrat@live.com>
2021-11-30 22:48:32 -0800

    aoc 2021 day 1 plus others

---
+++ aoc2021/1/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define MOD 1000000007
+using namespace std;
+
+ull partOne(vector<ull>& v) {
+    ull increases = 0;
+    for (int i = 1; i < v.size(); i++) {
+        if (v[i] > v[i - 1]) increases++;
+    }
+    return increases;
+}
+
+ull partTwo(vector<ull>& v) {
+    ull store = v[0] + v[1] + v[2];
+    ull ans = 0;
+
+    for (int i = 3; i < v.size(); i++) {
+        ull x = v[i] + v[i-1] + v[i-2];
+        if (x > store) ans++;
+        store = x;
+    }
+
+    return ans;
+}
+
+int main(int argc, char **argv) {
+    ifstream fin("input.txt", ios::in);
+    vector<ull> v;
+    
+    while (!fin.eof()) {
+        int x;
+        fin >> x;
+        v.push_back(x);
+    }
+
+    cout << partOne(v) << endl;
+    cout << partTwo(v) << endl;
+    return 0;
+}
