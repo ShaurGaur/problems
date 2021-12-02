2c9bf84f2b2d2d69983d74c4f312f737e86223fd
ShaurGaur <shauryavrat@live.com>
2021-12-02 14:57:50 -0800

    created aoc template

---
+++ aoc2021/0_template/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define MOD 1000000007
+using namespace std;
+
+vector<ull> v;
+
+ull partOne() {
+    return 0;
+}
+
+ull partTwo() {
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    string input_txt = "";
+    if (argc < 2) {
+        cout << "file: ";
+        cin >> input_txt;
+    }
+    else input_txt = string(argv[1]);
+    ifstream fin(input_txt.c_str(), ios::in);
+    
+    while (!fin.eof()) {
+        
+    }
+
+    cout << partOne() << endl;
+    cout << partTwo() << endl;
+    return 0;
+}
